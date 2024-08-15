93d7d999acb1194d43b29d6bd31ad024f36f9d14
Daniel <dhmmingway@gmail.com>
2024-08-15 18:27:04 +0100

    Added all of the code to make the game play once.

--- script.js
+++ script.js
@@ -1,2 +1,62 @@
-alert('This is a webiste template, you should proabibly change it. ;)')
-console.log('Hello world.\nI see you here >:)')
+var player = ''
+var computer = ''
+var streak = 0
+
+function get_user_choice(){
+    var u_choice = ''
+    while (u_choice != 'R' && u_choice != 'P' && u_choice != 'S'){
+        u_choice = prompt('(R)ock, (P)aper or (S)cissors: ')
+        u_choice = u_choice.toUpperCase()
+    }
+    return u_choice
+}
+
+function get_computer_choice() {
+    var choice = ''
+    var num = Math.random() * 10
+    if (num < 3){
+        choice = 'R'
+    }
+    else if (num >= 3 && num < 6){
+        choice = 'P'
+    }
+    else {
+        choice = 'S'
+    }
+    return choice
+}
+
+function compare_choices(p, c){
+    if (p === c){
+        console.log('Draw')
+    }
+    else if (p === 'R' && c === 'S'){
+        streak = streak++
+        console.log('You win! Your streak is:', streak)
+        
+    }
+    else if (p === 'P' && c === 'R'){
+        streak = streak++
+        console.log('You win! Your streak is:', streak)
+    }
+    else if (p === 'S' && c === 'P'){
+        streak = streak++
+        console.log('You win! Your streak is:', streak)
+    }
+    else if (p === 'R' && c === 'P'){
+        console.log('You loose! Beter luck next time.')
+    }
+    else if (p === 'P' && c === 'S'){
+        console.log('You loose! Beter luck next time.')
+    }
+    else {
+        console.log('You loose! Beter luck next time.')
+    }
+}
+
+'Main'
+player = get_user_choice()
+computer = get_computer_choice()
+console.log('Player choice:', player)
+console.log('Computer choice:', computer)
+compare_choices(player, computer)
